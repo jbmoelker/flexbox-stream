541d0b616728ac225bb6fb92d3f893a72660a912
Thadee Trompetter <thadee@2dehands.be>
2016-05-12 11:55:03 +0200

    append tail html on stream end outside contentStream fn

--- index.js
+++ index.js
@@ -7,24 +7,14 @@ const fs = require('fs');
 const app = express();
 const port = 3539; // "flex" in T9
 
-const files = [
-    {name: 'src/hero.html', order: 1, delay: 1750 },
-    {name: 'src/diagram.html', order: 2, delay: 2500 },
-    {name: 'src/article.html',  order: 3, delay: 900 }
-];
-
-const tail = '</main></body></html>';
-
 const timestamp = (timeStarted) => ((Date.now() - timeStarted) / 1000) + 's';
 
-
-function contentStream(files, tail, startTime) {
+function contentStream(files, startTime) {
     return es.readable(function (count, cb) {
         const stream = this;
         const file = files[count];
 
         if(!file){
-            stream.emit('data', tail);
             return stream.emit('end');
         }
 
@@ -51,6 +41,12 @@ function orderHtml(html, order, timestamp) {
 app.get('/', (req, res) => {
 
     const timeStarted = Date.now();
+    const files = [
+        {name: 'src/hero.html', order: 1, delay: 1750 },
+        {name: 'src/diagram.html', order: 2, delay: 2500 },
+        {name: 'src/article.html',  order: 3, delay: 900 }
+    ];
+    const tail = '</main></body></html>';
 
     res.write('<html lang="en">');
     res.write(fs.readFileSync('src/head.html')); // contains are `.flexbox-order` styles
@@ -59,7 +55,9 @@ app.get('/', (req, res) => {
     res.write(fs.readFileSync('src/footer.html'));
     res.write('<main class="flexbox-order">');
 
-    contentStream(files, tail, timeStarted).pipe(res);
+    contentStream(files, timeStarted).pipe(res).on('end', () => {
+        res.write(tail);
+    });
 });
 
 app.listen(port, () => console.log('Demo server available on http://localhost:' + port));
